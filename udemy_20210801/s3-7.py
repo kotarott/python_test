b0012d5bd798555a73a61219384e2b3db1f9071f
kotarott <kg221kg@gmail.com>
2021-08-02 22:59:31 +0900

    lib test

---
+++ udemy_20210801/s3-7.py
@@ -0,0 +1,14 @@
+# 標準出力
+print('hello world')
+
+name = input('名前を入力してください')
+print(name)
+
+age = 18
+
+# format関数
+print('私の名前は={}, 年齢は{}'.format(name, age))
+
+# 複数の変数に値を入力
+a = b = "ok"
+print(a, b)
